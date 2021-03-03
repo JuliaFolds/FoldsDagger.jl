463a98319cba0b6d3bc301abb6c10f8024b8095f
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-02 21:38:15 -0500

    Always test with multiple processes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 module TestFoldsDagger
+using Distributed: addprocs, nprocs
 using Test
+
+if get(ENV, "CI", "false") == "true"
+    addprocs(1)
+end
+@info "Testing with:" nprocs()
 
 @testset "$file" for file in sort([
     file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
