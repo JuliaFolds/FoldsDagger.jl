91d6ce2ae348788d2a34560bbde68b247e0da6ce
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-02 21:54:00 -0500

    Move test code to a package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,2 @@
-module TestFoldsDagger
-using Distributed: addprocs, nprocs
-using Test
-
-if get(ENV, "CI", "false") == "true"
-    addprocs(1)
-end
-@info "Testing with:" nprocs()
-
-@testset "$file" for file in sort([
-    file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
-])
-    include(file)
-end
-end  # module
+include("load.jl")
+TestFoldsDagger.runtests()
