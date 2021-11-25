6aff2b9f3840f12db6f1ebd6db1c8033fe35eef3
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-24 23:05:56 -0500

    DEBUG: Import Pkg at top-level

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
-if get(ENV, "CI", "false") == "true"
-    const Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
-    Pkg.pkg"add TestFunctionRunner#fix-distributed"
+const Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
+Pkg.pkg"add TestFunctionRunner#fix-distributed"
 
+if get(ENV, "CI", "false") == "true"
     using Distributed
     let n = 2
         if nprocs() < n
